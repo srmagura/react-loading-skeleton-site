63dc3ad1d35de48e9be6cebf42de9ee222b0e0ed
Rosie Magura <Rosie.Magura@iticentral.com>
2021-10-15 10:09:49 -0400

    Styling for post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -14,31 +14,35 @@ export const Post = ({ loading }: PostProps): React.ReactElement => (
       {loading ? (
         <div id="purple-border">
           <div className="grid-item">
-            <div className="column1">
-              <Skeleton height={62} width={62} circle={true} />
-              <Skeleton />
-            </div>
-            <div className="column2">
-              <Skeleton />
-              <Skeleton />
+            <div className="internal-grid">
+              <div className="column1">
+                <Skeleton height={68} width={68} circle={true} />
+                <Skeleton width={73} height={23} style={{ margin: "17px" }} />
+              </div>
+              <div className="column2">
+                <Skeleton height={27} style={{ marginBottom: "27px" }} />
+                <Skeleton count={4} />
+              </div>
             </div>
           </div>
         </div>
       ) : (
         <div id="blue-border">
           <div className="grid-item">
-            <div>
-              <FontAwesomeIcon icon={faSmile} size="4x" />
-              <p>John Doe</p>
-            </div>
-            <div>
-              <h3>Use React Loading Skeleton!</h3>
-              <p style={{ width: "500px" }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum nec justo feugiat, auctor nunc ac, volutpat arcu.
-                Suspendisse faucibus aliquam ante, sit amet iaculis dolor
-                posuere et. In ut placerat leo.
-              </p>
+            <div className="internal-grid">
+              <div className="column1">
+                <FontAwesomeIcon icon={faSmile} size="4x" />
+                <p>John Doe</p>
+              </div>
+              <div className="column2">
+                <h3>Use React Loading Skeleton!</h3>
+                <p style={{ width: "500px" }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum nec justo feugiat, auctor nunc ac, volutpat arcu.
+                  Suspendisse faucibus aliquam ante, sit amet iaculis dolor
+                  posuere et. In ut placerat leo.
+                </p>
+              </div>
             </div>
           </div>
         </div>
